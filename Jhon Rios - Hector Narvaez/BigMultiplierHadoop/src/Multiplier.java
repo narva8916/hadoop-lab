a00bd66a9f63ce65db9823a7c0043c23ec60da8c
Héctor Narváez <narva8916@hotmail.com>
2013-03-09 10:36:43 -0500

    Laboratorio Hadoop
    Carpeta BigMultiplierHadoop
    
    Contiene el código en Java de un ejercicio en Map Reduce que suma
    números que están en una cadena de entrada.
    Los ejemplos de los Input están aquí:
    BigMultiplierHadoop\InputFiles\Tarea\MultiplierInput
    
    Carpeta CountLetterHadoop
    
    Cotienen el código en Java de un ejercicio de Map Reduce que cuenta la
    cantidad de apariciones de una letra
    en unos textos.
    Los ejemplos de los Input están aquí:
    CountLetterHadoop\InputFiles\dataset

---
+++ Jhon Rios - Hector Narvaez/BigMultiplierHadoop/src/Multiplier.java
@@ -0,0 +1,75 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+
+public class Multiplier 
+{
+	public static class Map extends Mapper<LongWritable, Text, Text, LongWritable> {
+	    private  Text number = new Text();
+	        
+	    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+	        String line = value.toString();
+	        String[] numContent = line.split("\\+"); 
+	         
+	        for(int i = 0; i < numContent.length;i++)
+	        {	
+	        	number.set("one");
+	        	LongWritable lw = new LongWritable(Long.parseLong(numContent[i]));
+	        	
+	        	context.write(number,lw);
+	        }
+	    }
+	 } 
+	        
+	 public static class Reduce extends Reducer<Text, LongWritable, Text, LongWritable> {
+
+	    public void reduce(Text key, Iterable<LongWritable> values, Context context) 
+	      throws IOException, InterruptedException {
+	        Long mult = (long) 0;
+	        //for (IntWritable val : values) {
+	        //    sum += val.get();
+	        //}
+	        for(LongWritable val: values)
+	        {
+	        	mult += val.get();
+	        }
+	        Text cosanostra = new Text();
+	        cosanostra.set("resultado");
+	        context.write(cosanostra, new LongWritable(mult));
+	        
+	    }
+	 }
+	        
+	 public static void main(String[] args) throws Exception {
+	    Configuration conf = new Configuration();
+	        
+	    Job job = new Job(conf, "Multiplier");
+	    job.setJarByClass(Multiplier.class);
+	    job.setOutputKeyClass(Text.class);
+	    job.setOutputValueClass(LongWritable.class);
+	        
+	    job.setMapperClass(Map.class);
+	    job.setReducerClass(Reduce.class);
+	        
+	    job.setInputFormatClass(TextInputFormat.class);
+	    job.setOutputFormatClass(TextOutputFormat.class);
+	        
+	    FileInputFormat.addInputPath(job, new Path(args[0]));
+	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+	        
+	    job.waitForCompletion(true);
+	 }
+
+
+}
